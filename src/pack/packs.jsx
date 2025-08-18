dac7c1f0b6e01e7c0b4346450dd4a7c4ab97abae
Bruhh1369 <bruhh136969@gmail.com>
2025-08-18 17:14:14 +0700

    carousel

--- src/pack/packs.jsx
+++ src/pack/packs.jsx
@@ -6,7 +6,7 @@ import { GreenButton } from '@buttons';
 const packs = {
     "bedrock-ui-plus-b69-2": {
         title: "Bedrock UI+",
-        pictures: ["/post-images/bedrockuiplus.png", "/post-images/hudplusreworked.png"],
+        pictures: ["/post-images/bedrockuiplus.png", "/post-images/hudplusreworked.png", "/post-images/bedrockuiplus.png","/post-images/bedrockuiplus.png","/post-images/bedrockuiplus.png","/post-images/bedrockuiplus.png",],
         description: "A modern UI pack that enhances the user interface of Minecraft Bedrock Edition."
     },
     "improved-plus-b69": {
@@ -25,49 +25,19 @@ const packs = {
 const PacksInfo = () => {
     const { slug } = useParams();
     const pack = packs[slug];
-    const [currentIndex, setCurrentIndex] = React.useState(0);
 
     if (!pack) {
         return <div className="pack-not-found">Pack not found</div>;
     }
 
-    const pictures = pack.pictures || [];
-    const hasPictures = pictures.length > 0;
-
-    const handlePrev = () => {
-        setCurrentIndex((prev) => (prev === 0 ? pictures.length - 1 : prev - 1));
-    };
-
-    const handleNext = () => {
-        setCurrentIndex((prev) => (prev === pictures.length - 1 ? 0 : prev + 1));
-    };
-
     return (
         <div className="pack-info">
             <div className="carousel">
-                {hasPictures && (
-                    <>
-                        <img className="carousel-img" src={pictures[currentIndex]} alt={pack.title} />
-                    </>
-                )}
-            </div>
-            <div className="carousel-btns">
-                {hasPictures && (
-                    <>
-                        <button className="carousel-btn left" onClick={handlePrev}>&lt;</button>
-                        <button className="carousel-btn right" onClick={handleNext}>&gt;</button>
-                    </>
-                )}
-            </div>
-            <div className="pack-desc">
-                <h1>{pack.title}</h1>
-                <p>{pack.description}</p>
-            </div>
-            <div className="download">
-                <GreenButton
-                    hyperLink={`https://mcpedl.com/${slug}`}
-                    childElement={<p>Download</p>}
-                />
+                {pack.pictures.map((picture) => (
+                    <div className="carousel-card">
+                        <img src={picture} alt={pack.title} className="carousel-image" />
+                    </div>
+                ))}
             </div>
         </div>
     );
